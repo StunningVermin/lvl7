1f618a85ad27cee90168dbb02d80cbee1edc0e1d
StunningVermin <111050411+StunningVermin@users.noreply.github.com>
2022-12-03 00:05:54 +0530

    lvl10

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const app = require("./app");
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`server listening at port - ${port}`);
+const app = require("./app.js");
+const express = require("express");
+const path = require("path");
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Started express server at port 8000");
 });
